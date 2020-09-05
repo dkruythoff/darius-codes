172cd2339279bac8dad2a81d2a7646d3f44632cb
Darius Kruythoff <dkruythoff@gmail.com>
2020-09-05 23:33:53 +0200

    added foolishly forgotten syntax highlighting

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 const pluginRss = require("@11ty/eleventy-plugin-rss")
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight")
 const sass = require('sass')
 const path = require('path')
 
@@ -20,7 +21,9 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget(path.join(dirRoot, '_scss'))
 
   eleventyConfig.addPassthroughCopy({ "static/fonts": "fonts" })
+  eleventyConfig.addPassthroughCopy({ "static/prism-vsc-dark-plus.css": "prism-vsc-dark-plus.css" })
   eleventyConfig.addPlugin(pluginRss)
+  eleventyConfig.addPlugin(syntaxHighlight)
   eleventyConfig.setDataDeepMerge(true)
 
   return {
